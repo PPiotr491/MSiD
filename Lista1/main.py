f9ad7b7a6542872c850ba9fdc7846232a8087069
PPiotr491 <ppiotrowski491@gmail.com>
2026-03-23 23:15:33 +0100

    initial commit

---
+++ Lista1/main.py
@@ -0,0 +1,38 @@
+import kagglehub
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from kagglehub import KaggleDatasetAdapter
+
+# # Download latest version
+# path = kagglehub.dataset_download("sehaj1104/student-mental-health-and-burnout-dataset", output_dir="C:/Users/Super/PycharmProjects/MSiD/resources")
+#
+# print("Path to dataset files:", path)
+
+file_path = "C:/Users/Super/PycharmProjects/MSiD/resources/student_mental_health_burnout.csv"
+
+# df = kagglehub.dataset_load(KaggleDatasetAdapter.PANDAS,
+#                             "sehaj1104/student-mental-health-and-burnout-dataset",
+#                             file_path,
+#                             )
+
+df = pd.read_csv(file_path, )
+
+print("First 5 records:", df.head())
+
+# pandas.set_option('display.max_columns', None)
+# print(df.loc[0:5])
+
+print(df.info())
+
+pd.set_option('display.max_columns', None)
+print(df.describe())
+
+sns.set_theme(style="whitegrid")
+plt.figure(figsize=(12, 5))
+
+plt.subplot(1, 2, 1)
+sns.histplot(df['age'].dropna(), kde=True, color='skyblue')
+plt.title('Rozkład wieku pasażerów')
+
+
